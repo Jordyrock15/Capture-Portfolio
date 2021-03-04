3ef1235a73d85f8b76cbc2b046f99d91be698929
Jordan Barrand <Jordan.barrand@outlook.com>
2021-03-04 18:59:30 +0000

    Page Transition & about/wave anim

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -4,28 +4,31 @@ import { Link } from 'react-router-dom';
 import athlete from '../img/athlete-small.png';
 import theracer from '../img/theracer-small.png';
 import goodtimes from '../img/goodtimes-small.png';
+//Animation
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
 
 const OurWork = () => {
 	return (
-		<Work>
+		<Work variants={pageAnimation} initial='hidden' animate='show' exit='exit' style={{ background: '#fff' }}>
 			<Movie>
 				<h2>The Athlete</h2>
 				<div className='line'></div>
-				<Link>
+				<Link to='/work/the-athlete'>
 					<img src={athlete} alt='athlete' />
 				</Link>
 			</Movie>
 			<Movie>
 				<h2>The Racer</h2>
 				<div className='line'></div>
-				<Link>
+				<Link to='/work/the-racer'>
 					<img src={theracer} alt='theracer' />
 				</Link>
 			</Movie>
 			<Movie>
 				<h2>Good Times</h2>
 				<div className='line'></div>
-				<Link>
+				<Link to='/work/good-times'>
 					<img src={goodtimes} alt='goodtimes' />
 				</Link>
 			</Movie>
@@ -33,7 +36,7 @@ const OurWork = () => {
 	);
 };
 
-const Work = styled.div`
+const Work = styled(motion.div)`
 	min-height: 100vh;
 	overflow: hidden;
 	padding: 5rem 10rem;
